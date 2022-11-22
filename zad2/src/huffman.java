766aa686df0ee882879c4c49c3b9009890f8a4c9
norbert753 <92160471+norbert753@users.noreply.github.com>
2022-11-22 11:14:55 +0100

    zad2
    zad2

--- zad2/src/huffman.java
+++ zad2/src/huffman.java
@@ -1,44 +1,67 @@
-import java.util.*;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class huffman {
-    public static void main(String[] args){
+
+        import java.util.*;
+        import java.util.stream.Collectors;
+
+        public class huffman {
         Scanner cin = new Scanner(System.in);
-        String znaki;
-        System.out.println("Podaj znaki");
-        znaki = cin.nextLine();
-        System.out.println(liczenieZnakow(znaki));
-
+        String ciagZnakow;
+        public void wypisz(){
+        System.out.print("Podaj ciag znakow: ");
+        ciagZnakow = cin.nextLine();
+        Map<Character, Long> wysapienia = zliczWystapienia(ciagZnakow);
+        System.out.println(wysapienia);
+        Map<Character, String> binarMap = zamNaBin(wysapienia);
+        System.out.println(binarMap);
+        String encryptedMessage = zaszyfruj(binarMap, ciagZnakow);
+        System.out.println("Zaszyfrowana wiad: " + encryptedMessage);
+        System.out.println("Odszyfrowana wiad: " + odszyfruj(binarMap, encryptedMessage));
     }
-    public static  Map<Character, Integer> liczenieZnakow (String znaki){
-        HashSet<Character> usundupli = new HashSet<>(); //usuwanie duplikatow
-        Map<Character, Integer> mapString= new HashMap<>();
-        for(int i=0; i< znaki.length(); i++){
-            usundupli.add(znaki.charAt(i));
-        }
-        for(Character znak: usundupli){
-            int j = 0 ;
-            for (Character compare : znaki.toCharArray()){
-                if(compare.equals(znak)){
-                    j++;
-                }
+
+    public  Map<Character, Long> zliczWystapienia(String ciagZnakow){
+        Map <Character, Long> mapStringToCount = ciagZnakow
+                .chars().mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+        return sort(mapStringToCount);
+    }
+
+    public  Map<Character, Long> sort(Map<Character, Long> mapa){
+        LinkedHashMap<Character, Long> result = new LinkedHashMap<>();
+        mapa.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEachOrdered(value -> result.put(value.getKey(), value.getValue()));
+
+        return result;
+    }
+
+    public  Map<Character, String> zamNaBin(Map<Character, Long> mapa){
+        Map<Character, String> wartoscBinarna = new LinkedHashMap<>();
+        int i = mapa.size()-1;
+        String bin = "1";
+        for (Character wystapienie: mapa.keySet()){
+            if(i == 0){
+                wartoscBinarna.put(wystapienie, "0");
             }
-            mapString.put(znak, j);
+            else {
+                wartoscBinarna.put(wystapienie, bin.repeat(i) + "0");
+            }
+            i--;
         }
-        return sotow(mapString);
-    }
-    public static  Map<Character, Integer> sotow(Map<Character, Integer>mapa){
-        List<Map.Entry<Character, Integer>> sotow = new ArrayList<>(mapa.entrySet());
-         sotow.sort(Map.Entry.comparingByValue());                                     //sortowanie
-         Map<Character, Integer> wynsor = new LinkedHashMap<>();
-         for ( Map.Entry<Character, Integer> pen1 : sotow) {
-         wynsor.put(pen1.getKey(),pen1.getValue());
-
-        }
-    return wynsor;
+        return wartoscBinarna;
     }
 
+    public  String zaszyfruj(Map<Character, String> wartoscBinarna, String wiadomosc){
+        for (Map.Entry<Character, String> szyfr : wartoscBinarna.entrySet()){
+            wiadomosc = wiadomosc.replace(szyfr.getKey().toString(), szyfr.getValue());
+        }
+        return wiadomosc;
+    }
 
+    private  String odszyfruj(Map<Character, String> wartoscBinarna, String encryptedMessage){
+        for (Map.Entry<Character, String> szyfr : wartoscBinarna.entrySet()){
+            encryptedMessage = encryptedMessage.replace(szyfr.getValue(), szyfr.getKey().toString());
+        }
+        return encryptedMessage;
+    }
 }
